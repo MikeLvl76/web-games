b3da297ba76c8762a67490d1d7fb8e64da486bf4
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-10-06 18:40:46 +0200

    implement cell class

--- src/app/(games)/maze/page.tsx
+++ src/app/(games)/maze/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import P5Sketch from "@/components/ui/p5-sketch";
+import { Cell } from "@/utils/p5/maze/cell";
 import p5 from "p5";
 
 export default function MazePage() {
   const sketch = (p: p5) => {
+    let cell: Cell;
+
     p.setup = () => {
       const div = document.getElementById("p5-container");
       p.createCanvas(
@@ -12,12 +15,15 @@ export default function MazePage() {
         div?.offsetHeight ?? p.windowHeight * 0.8
       );
       p.background(0);
+      cell = new Cell(p, [p.width * 0.5, p.height * 0.5], [40, 40], "normal");
     };
 
     p.draw = () => {
       p.background(0);
-      p.fill(255);
-      p.ellipse(p.width * 0.5, p.height * 0.5, 20, 20);
+
+      if (cell) {
+        cell.draw();
+      }
     };
   };
 
---
+++ src/utils/p5/maze/cell.ts
@@ -0,0 +1,81 @@
+import p5 from "p5";
+
+type CellType = "normal" | "entry" | "exit";
+type CellWallsDisplay = {
+  left: boolean;
+  right: boolean;
+  top: boolean;
+  bottom: boolean;
+};
+
+export class Cell {
+  p: p5;
+  position: [number, number];
+  dimensions: [number, number];
+  type: CellType;
+  isVisited: boolean;
+  walls: CellWallsDisplay;
+
+  constructor(
+    _p: p5,
+    _position: [number, number],
+    _dimensions: [number, number],
+    _type: CellType
+  ) {
+    this.p = _p;
+    this.position = _position;
+    this.dimensions = _dimensions;
+    this.type = _type;
+    this.isVisited = false;
+    this.walls = {
+      left: true,
+      right: true,
+      top: true,
+      bottom: true,
+    };
+  }
+
+  getOppositeWallName(name: keyof CellWallsDisplay): keyof CellWallsDisplay {
+    if (name === "left") return "right";
+    if (name === "right") return "left";
+    if (name === "bottom") return "top";
+    if (name === "top") return "bottom";
+    return name;
+  }
+
+  draw() {
+    this.p.stroke(127);
+    this.p.strokeWeight(2);
+
+    const [x, y] = this.position;
+    const [w, h] = this.dimensions;
+
+    if (this.walls.left) {
+      this.p.line(x, y, x, y + h);
+    }
+
+    if (this.walls.right) {
+      this.p.line(x + w, y, x + w, y + h);
+    }
+
+    if (this.walls.top) {
+      this.p.line(x, y, x + w, y);
+    }
+
+    if (this.walls.bottom) {
+      this.p.line(x, y + h, x + w, y + h);
+    }
+
+    if (this.type !== "normal") {
+      this.p.noStroke();
+      if (this.type === "entry") {
+        this.p.fill(2, 224, 36);
+      } else {
+        this.p.fill(235, 64, 12);
+      }
+      this.p.rect(x, y, w, h);
+    }
+  }
+}
+
+export type { CellType, CellWallsDisplay };
