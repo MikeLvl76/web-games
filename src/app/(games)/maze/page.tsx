79b92c4c24b2372a6281a5a39ca4c306af497c45
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-10-08 20:21:23 +0200

    fix coutndown issueand refactor CSS

--- src/app/(games)/maze/page.tsx
+++ src/app/(games)/maze/page.tsx
@@ -4,8 +4,9 @@ import P5Sketch from "@/components/ui/p5-sketch";
 import { stringifyTime } from "@/utils/misc/time-string";
 import { Maze } from "@/utils/p5/maze/maze";
 import { Player } from "@/utils/p5/maze/player";
+import { RotateCcw } from "lucide-react";
 import p5 from "p5";
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 
 type Options = {
   size: number;
@@ -16,39 +17,52 @@ type Options = {
 
 export default function MazePage() {
   const [options, setOptions] = useState<Options>({
-    size: 30,
-    enableCountdown: false,
+    size: 40,
+    enableCountdown: true,
     enablePlayerPath: true,
     enableReset: true,
   });
   const [startGame, setStartGame] = useState(false);
   const [countdown, setCountdown] = useState<{ value: number; text: string }>({
-    value: 0,
-    text: "00:00",
+    value: 60 * Math.floor(options.size / 10),
+    text: stringifyTime(60 * Math.floor(options.size / 10)),
   });
+  const [gameOver, setGameOver] = useState(false);
+  const gameOverRef = useRef(false);
+  const timeRef = useRef(countdown);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const [refresh, setRefresh] = useState(0);
 
   useEffect(() => {
-    if (!options.enableCountdown) return;
+    timeRef.current = countdown;
+  }, [countdown]);
 
-    let time = 60 * Math.floor(options.size / 4);
+  useEffect(() => {
+    if (!options.enableCountdown || gameOver) return;
 
-    const interval = setInterval(() => {
-      time--;
-
-      setCountdown({
-        value: time,
-        text: stringifyTime(time),
-      });
+    let time = 60 * Math.floor(options.size / 10);
+    intervalRef.current = setInterval(() => {
+      time = Math.max(time - 1, 0);
+      setCountdown({ value: time, text: stringifyTime(time) });
     }, 1000);
 
-    return () => clearInterval(interval);
-  }, [options.enableCountdown, options.size]);
+    return () => clearInterval(intervalRef.current!);
+  }, [gameOver, options.enableCountdown, options.size]);
+
+  useEffect(() => {
+    if (countdown.value === 0) {
+      clearInterval(intervalRef.current!);
+      intervalRef.current = null;
+      gameOverRef.current = true;
+      setGameOver(true);
+    }
+  }, [countdown.value]);
 
   const sketch = useCallback(
     (p: p5) => {
       let maze: Maze;
       let player: Player;
-      const defaultCountdown = countdown.value;
+      const defaultCountdown = timeRef.current.value;
 
       p.setup = () => {
         const div = document.getElementById("p5-container");
@@ -73,6 +87,8 @@ export default function MazePage() {
 
         if (player.exitFound) {
           p.noLoop();
+          gameOverRef.current = true;
+          p.noStroke();
           p.background(0);
           p.textAlign(p.CENTER);
           p.fill(0, 255, 0);
@@ -83,15 +99,14 @@ export default function MazePage() {
           p.textSize(32);
           p.text(
             `You found the exit in ${stringifyTime(
-              defaultCountdown - countdown.value
+              defaultCountdown - timeRef.current.value
             )} by moving ${player.movesCount} times!`,
             p.width * 0.5,
             p.height * 0.7
           );
-        }
-
-        if (countdown.value === 0) {
+        } else if (gameOverRef.current) {
           p.noLoop();
+          p.noStroke();
           p.background(0);
           p.textAlign(p.CENTER);
           p.fill(255, 0, 0);
@@ -116,12 +131,12 @@ export default function MazePage() {
         }
       };
     },
-    [countdown, options.enablePlayerPath, options.enableReset, options.size]
+    [options.enablePlayerPath, options.enableReset, options.size]
   );
 
   const OptionsMenu = memo(
     ({ enableCountdown, enablePlayerPath, enableReset, size }: Options) => (
-      <div className="flex flex-col items-center gap-4 w-[40vw] h-[40vh] rounded-md p-2">
+      <div className="flex flex-col items-center gap-4 w-[40vw] h-[40vh] rounded-md p-2 select-none">
         <div className="flex flex-row justify-between items-center w-1/4">
           <label>Enable countdown</label>
           <input
@@ -169,28 +184,30 @@ export default function MazePage() {
             Size ({size}x{size})
           </label>
           <div className="flex flex-row justify-center items-center ml-4 w-full gap-2">
-            <label>20</label>
+            <label>30</label>
             <input
               type="range"
               value={size}
-              min={20}
+              min={30}
               step={1}
-              max={40}
-              onChange={(e) => {
-                e.preventDefault();
+              max={50}
+              onChange={(e) =>
                 setOptions((prev) => ({
                   ...prev,
-                  size: parseInt(e.target.value),
-                }));
-              }}
+                  size: Number(e.target.value),
+                }))
+              }
               className="hover:cursor-grab"
             />
-            <label>40</label>
+            <label>50</label>
           </div>
         </div>
         <div className="flex justify-center mt-4 w-full">
           <button
-            onClick={() => setStartGame(true)}
+            onClick={() => {
+              setStartGame(true);
+              gameOverRef.current = false;
+            }}
             className="w-fit h-fit p-2 bg-blue-500 text-white rounded-md hover:cursor-pointer"
           >
             Start game
@@ -205,7 +222,7 @@ export default function MazePage() {
     <div className="flex flex-row gap-2">
       {startGame ? (
         <>
-          <P5Sketch sketch={sketch} />
+          <P5Sketch sketch={sketch} refresh={refresh} />
           <div className="flex flex-col items-start gap-4 w-[15vw]">
             <span className="font-bold text-3xl">Find exit.</span>
             <div className="w-full">
@@ -243,12 +260,40 @@ export default function MazePage() {
                 <p className="text-red-600 font-bold">Disabled</p>
               )}
             </div>
-            <button
-              onClick={() => setStartGame(false)}
-              className="w-fit h-fit p-2 bg-blue-500 text-white font-bold rounded-md hover:cursor-pointer"
-            >
-              Return to menu
-            </button>
+            <div className="flex flex-row items-center justify-evenly w-full">
+              <button
+                onClick={() => {
+                  setStartGame(false);
+                  clearInterval(intervalRef.current!);
+                  intervalRef.current = null;
+                  gameOverRef.current = false;
+                  setGameOver(false);
+                  setCountdown({
+                    value: 60 * Math.floor(options.size / 10),
+                    text: stringifyTime(60 * Math.floor(options.size / 10)),
+                  });
+                }}
+                className="w-fit h-fit p-2 bg-green-700 text-white font-bold rounded-md hover:cursor-pointer"
+              >
+                Return to menu
+              </button>
+              <RotateCcw
+                color="white"
+                size={24}
+                onClick={() => {
+                  setRefresh((prev) => prev + 1);
+                  clearInterval(intervalRef.current!);
+                  intervalRef.current = null;
+                  gameOverRef.current = false;
+                  setGameOver(false);
+                  setCountdown({
+                    value: 60 * Math.floor(options.size / 10),
+                    text: stringifyTime(60 * Math.floor(options.size / 10)),
+                  });
+                }}
+                className="self-start w-fit h-fit p-2 bg-blue-400 rounded-md hover:cursor-pointer"
+              />
+            </div>
           </div>
         </>
       ) : (
