ed7e003a20dfecd722c3ba83e422cb8f318a9521
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-10-17 17:12:10 +0200

    refactor game stats to be more efficient

--- src/app/(games)/checkers/page.tsx
+++ src/app/(games)/checkers/page.tsx
@@ -4,6 +4,8 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { Board } from "./board";
 import { GameStatus } from "./game-status";
 import { stringifyTime } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { RotateCcw } from "lucide-react";
 
 export type Tile = {
   piece?: "pawn" | "dame";
@@ -385,43 +387,66 @@ export default function CheckersPage() {
           onClick={handleClick}
         />
       </div>
-
       <div className="flex w-[20%]">
         <GameStatus
-          playtime={timer.text}
-          playerColor={playerTurnColor!}
-          winner={winner}
-          onRestart={() => {
-            init();
-            setCanContinue(false);
-            setPlayerTurnColor(() => (winner === "black" ? "white" : "black"));
-            setWinner(undefined);
-            setSelectedIdx(-1);
-            setNextMove({ indices: [], captures: [] });
-            setTimer({ value: 0, text: stringifyTime(0) });
-            clearInterval(intervalRef.current!);
-            intervalRef.current = setInterval(() => {
-              setTimer((prev) => ({
-                value: prev.value + 1,
-                text: stringifyTime(prev.value + 1),
-              }));
-            }, 1000);
-          }}
-          otherComponent={
-            canContinue && (
-              <button
-                onClick={() => {
-                  setCanContinue(false);
-                  setPlayerTurnColor((prev) =>
-                    prev === "black" ? "white" : "black"
-                  );
-                }}
-                className="w-fit h-fit p-2 rounded-sm bg-red-500 text-white hover:cursor-pointer"
-              >
-                End turn
-              </button>
-            )
-          }
+          infos={[
+            {
+              label: "Current turn",
+              value: playerTurnColor === "black" ? "Player 1" : "Player 2",
+            },
+            { label: "Game time", value: timer.text },
+            {
+              label: "Winner",
+              value: winner
+                ? winner === "black"
+                  ? "Player 1"
+                  : "Player 2"
+                : "/",
+            },
+          ]}
+          options={[
+            <Button
+              key="end-turn-button"
+              onClick={() => {
+                setCanContinue(false);
+                setPlayerTurnColor((prev) =>
+                  prev === "black" ? "white" : "black"
+                );
+              }}
+              className="w-fit h-fit p-2 rounded-sm bg-red-500 text-sm text-white font-bold hover:cursor-pointer disabled:opacity-20"
+              disabled={!canContinue}
+            >
+              End turn
+            </Button>,
+            <Button
+              key="restart-button"
+              variant="default"
+              className="flex w-fit h-fit p-2 bg-blue-400 rounded-md hover:cursor-pointer"
+              onClick={() => {
+                init();
+                setCanContinue(false);
+                setPlayerTurnColor(() =>
+                  winner === "black" ? "white" : "black"
+                );
+                setWinner(undefined);
+                setSelectedIdx(-1);
+                setNextMove({ indices: [], captures: [] });
+                setTimer({ value: 0, text: stringifyTime(0) });
+                clearInterval(intervalRef.current!);
+                intervalRef.current = setInterval(() => {
+                  setTimer((prev) => ({
+                    value: prev.value + 1,
+                    text: stringifyTime(prev.value + 1),
+                  }));
+                }, 1000);
+              }}
+            >
+              <p className="text-white font-bold text-md text-center">
+                Restart
+              </p>
+              <RotateCcw color="white" size={32} />
+            </Button>,
+          ]}
         />
       </div>
     </div>
